8bbc092f91afb622e7e5880a04553dae6c0dbc44
Yaremchuk <a.yaremchuk911@gmail.com>
2022-12-13 18:30:24 +0200

    react-redux mini app

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {compose, createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import App from './App'
+import {rootReducer} from './redux/rootReducer'
+import {forbiddenWordsMiddleware} from './redux/middleware'
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(
+    thunk, forbiddenWordsMiddleware
+  ),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
+
+
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+render(app, document.getElementById('root'));
+
+
+
